05a82572d4cb86208907508839e466e32ecd4996
Jordan Faas-Bush <jmanfb@gmail.com>
2019-01-27 16:43:17 -0500

    Added a basic guard thing

---
+++ Assets/Editor/GuardEditor.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(GuardScript))]
+public class GuardEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        // draw lines between each of the points so we know the order
+        GuardScript guard = (GuardScript)target;
+        for (int i = 0; i < guard.positions.Count; i++)
+        {
+            Handles.DrawLine(guard.positions[i], guard.positions[(i + 1) % guard.positions.Count]);
+        }
+
+        EditorGUI.BeginChangeCheck();
+
+        for (int i = 0; i < Mathf.Min(guard.positions.Count, guard.rotations.Count); i++)
+        {
+            if (guard.editPositions)
+            {
+                // handles where each of the points are
+                guard.positions[i] = Handles.PositionHandle(guard.positions[i], guard.rotations[i]);
+            }
+            else
+            {
+                // handles for each of the rotations where the points are
+                guard.rotations[i] = Handles.RotationHandle(guard.rotations[i], guard.positions[i]);
+            }
+        }
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            // then save it all!
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        GuardScript guard = (GuardScript)target;
+
+        if (guard.rotations.Count != guard.positions.Count) {
+            Debug.LogError("Non-Equal number of rotations and positions");
+            return;
+        }
+
+        //// check if the user deleted or add points and handle that nicelyish.
+        //bool foundDifference = false;
+        //int brokenDetector = 0;
+        //do
+        //{
+        //    brokenDetector++;
+        //    if (brokenDetector > 100)
+        //    {
+        //        Debug.Log("Error loop forced break");
+        //        break;
+        //    }
+        //    foundDifference = false;
+        //    int posDifference = FindDifference(guard.BackupPositions, guard.positions);
+        //    int rotDifference = FindDifference(guard.BackupRotations, guard.rotations);
+        //    if (posDifference != -1)
+        //    {
+        //        if (guard.backupPositions.Count > guard.positions.Count)
+        //        {
+        //            // deleted
+        //            guard.backupPositions.RemoveAt(posDifference);
+        //            foundDifference = true;
+        //        }
+        //        else if (guard.positions.Count > 0)
+        //        {
+        //            // added
+        //            foundDifference = true;
+        //            if (posDifference == guard.backupPositions.Count)
+        //            {
+        //                guard.backupPositions.Add(guard.positions[posDifference]);
+        //            }
+        //            else
+        //            {
+        //                guard.backupPositions.Insert(posDifference, guard.positions[posDifference]);
+        //            }
+        //        }
+        //    }
+        //    if (rotDifference != -1)
+        //    {
+        //        if (guard.backupRotations.Count > guard.rotations.Count)
+        //        {
+        //            // deleted
+        //            foundDifference = true;
+        //            guard.backupRotations.RemoveAt(rotDifference);
+        //        }
+        //        else if (guard.rotations.Count > 0)
+        //        {
+        //            // added
+        //            foundDifference = true;
+        //            if (rotDifference == guard.backupRotations.Count)
+        //            {
+        //                guard.backupRotations.Add(guard.rotations[rotDifference]);
+        //            }
+        //            else
+        //            {
+        //                guard.backupRotations.Insert(rotDifference, guard.rotations[rotDifference]);
+        //            }
+        //        }
+        //    }
+        //} while (foundDifference); // so that we can handle multiple additions/removes
+
+
+        // then check if we should add more items I guess?
+
+        guard.editIndex = EditorGUILayout.DelayedIntField("Index:", guard.editIndex);
+        int index = guard.editIndex;
+        if (index < 0)
+        {
+            index = guard.positions.Count + index + 1; // so we can index from the back as well!
+        }
+        if (GUILayout.Button("Add"))
+        {
+            if (index >= 0 && index < guard.positions.Count)
+            {
+                guard.positions.Insert(index, Vector3.zero);
+                guard.rotations.Insert(index, Quaternion.identity);
+            } else if (index == guard.positions.Count)
+            {
+                guard.positions.Add(Vector3.zero);
+                guard.rotations.Add(Quaternion.identity);
+            }
+        }
+        else if (GUILayout.Button("Remove"))
+        {
+            if (index >= 0 && index < guard.positions.Count)
+            {
+                guard.positions.RemoveAt(index);
+                guard.rotations.RemoveAt(index);
+            }
+        }
+    }
+
+    private int FindDifference(List<Vector3> original, List<Vector3> newList)
+    {
+        int minCount = Mathf.Min(original.Count, newList.Count);
+        if (minCount == 0)
+        {
+            return 0;
+        }
+        // otherwise loop over until they aren't equivalent any more
+        // return the item index that isn't the same
+        for (int i = 0; i < minCount; i++)
+        {
+            if (original[i] != newList[i])
+            {
+                return i;
+            }
+        }
+        if (original.Count != newList.Count)
+        {
+            return minCount + 1;
+        }
+        return -1;
+    }
+
+    private int FindDifference(List<Quaternion> original, List<Quaternion> newList)
+    {
+        int minCount = Mathf.Min(original.Count, newList.Count);
+        if (minCount == 0)
+        {
+            return 0;
+        }
+        // otherwise loop over until they aren't equivalent any more
+        // return the item index that isn't the same
+        for (int i = 0; i < minCount; i++)
+        {
+            if (original[i] != newList[i])
+            {
+                return i;
+            }
+        }
+        if (original.Count != newList.Count)
+        {
+            return minCount + 1;
+        }
+        return -1;
+    }
+}
---
+++ Assets/Scripts/GuardScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GuardScript : MonoBehaviour
+{
+    public float speed = .5f; // meters per second
+    [Space]
+    public bool editPositions = true; // if false it edits rotations
+    public List<Vector3> positions = new List<Vector3>();
+    public List<Quaternion> rotations = new List<Quaternion>();
+
+    [HideInInspector]
+    public int editIndex = 0;
+
+    //[HideInInspector]
+    //public List<Vector3> backupPositions = new List<Vector3>(); // these are for the editor
+    //[HideInInspector]
+    //public List<Quaternion> backupRotations = new List<Quaternion>(); // these are for the editor
+
+    private int target;
+    private Quaternion rotation = Quaternion.identity;
+    private Quaternion previousRotation = Quaternion.identity;
+    float percentProgress = 0; // this is for lerping the quaternions
+    float percentSpeedThing = 1; // this is also for quaternions
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (positions.Count > 0)
+        {
+            target = 0;
+            transform.position = positions[target];
+            transform.rotation = rotations[target];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (positions.Count > 0)
+        {
+            percentProgress += Time.deltaTime / percentSpeedThing;
+            // move towards each of the positions in turn
+            transform.position = Vector3.MoveTowards(transform.position, positions[target], speed * Time.deltaTime);
+            //transform.rotation = Quaternion.Lerp(previousRotation, rotation, percentProgress);
+            if (Vector3.Distance(transform.position, positions[target]) != 0)
+            {
+                transform.LookAt(positions[target]);
+            }
+
+            if (transform.position == positions[target])
+            {
+                // then find the next target
+                target++;
+                target %= positions.Count;
+                rotation = rotations[target];
+                previousRotation = transform.rotation;
+                // now calculate the time to turn over
+                float distance = Vector3.Distance(transform.position, positions[target]);
+                percentSpeedThing = distance / speed;
+            }
+        }
+    }
+
+    public List<Vector3> BackupPositions
+    {
+        get;set;
+    }
+
+    public List<Quaternion> BackupRotations
+    {
+        get;set;
+    }
+}
