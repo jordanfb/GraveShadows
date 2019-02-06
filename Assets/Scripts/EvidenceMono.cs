708b94b4d39cabd887772de1b7ec6379f5b3a006
Eric Franco <france2@rpi.edu>
2019-02-06 15:42:06 -0500

    Can Display Object types via Sprite Rendering on Yarn Board

---
+++ Assets/Scripts/EvidenceMono.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvidenceMono : MonoBehaviour
+{
+    [SerializeField]
+    private Evidence _evidenceInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Might be necessary
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Might be necessary
+    }
+
+    public Evidence EvidenceInfo
+    {
+        get { return _evidenceInfo; }
+    }
+}
---
+++ Assets/Scripts/YarnBoard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YarnBoard : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pinPrefab;
+    private List<GameObject> _pins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pins = new List<GameObject>();
+        InitializePins();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitializePins()
+    {
+        foreach(GameObject go in PlayerManager.instance.CollectedEvidence) {
+            GameObject pin = Instantiate(_pinPrefab) as GameObject;
+            //TODO: Change transform position (need to decide best way of doing so)
+            _pins.Add(pin);
+
+            Evidence evidence = go.GetComponent<EvidenceMono>().EvidenceInfo;
+            switch(evidence.GetEvidenceType)
+            {
+                case EvidenceType.Object:
+                    //Display photo associated with evidence SO & parent it to pin
+                    GameObject photo = new GameObject(evidence.Name);
+                    photo.transform.parent = pin.transform;
+                    SpriteRenderer pSprite = photo.AddComponent<SpriteRenderer>();
+                    pSprite.sprite = evidence.Photo;
+                    break;
+                case EvidenceType.Conversation:
+                    break;
+                case EvidenceType.Document:
+                    break;
+            }
+        }
+    }
+}
