98a6ecafe83ce7823df62fb60d359a658c04edad
Eric Franco <france2@rpi.edu>
2019-02-06 15:01:05 -0500

    Added apple texture, merged master in

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance;
+
+    [SerializeField]
+    private List<GameObject> _collectedEvidence;
+
+    void Awake()
+    {
+        //This ensures we don't have multiple instances of the Player Manager
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public List<GameObject> CollectedEvidence
+    {
+        get { return _collectedEvidence; }
+    }
+
+    public void CollectEvidence(GameObject go)
+    {
+        _collectedEvidence.Add(go);
+    }
+}
