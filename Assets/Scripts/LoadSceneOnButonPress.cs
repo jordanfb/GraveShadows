29b63e27b51c6250978365550fff41ecd33652c6
Jordan Faas-Bush <jmanfb@gmail.com>
2019-02-13 14:14:29 -0500

    Added switching scenes

---
+++ Assets/Scripts/LoadSceneOnButonPress.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnButonPress : MonoBehaviour
+{
+    public KeyCode key;
+    public string scene;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
