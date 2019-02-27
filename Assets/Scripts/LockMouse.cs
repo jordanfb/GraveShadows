65fc1875ba039a537f4dc9e1f402808d1efde2c6
Jordan Faas-Bush <jmanfb@gmail.com>
2019-02-27 01:14:45 -0500

    Lock mouse to game window nicely

---
+++ Assets/Scripts/LockMouse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockMouse : MonoBehaviour
+{
+
+    public bool lockMouse = true;
+    public bool unlockOnEscape = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lockMouse)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && lockMouse)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && unlockOnEscape)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
