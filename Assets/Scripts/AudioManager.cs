cae10dcf14c3d5e70a625030db879a2126e84a44
Eric Franco <france2@rpi.edu>
2019-02-26 20:17:33 -0500

    Created audiomanager, play step sfx when player walks

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private simplePlayerMovement _player;
+    [SerializeField]
+    private ShadowRealmManager _srManager;
+    [SerializeField]
+    private AudioClip[] _clips;
+    private AudioSource _audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if((Input.GetAxis("Horizontal") != 0.0f || Input.GetAxis("Vertical") > 0.0f) &&
+            !_audioSource.isPlaying && !_srManager.isInShadowRealm)
+        {
+            _audioSource.clip = _clips[0];
+            _audioSource.Play();
+        }
+    }
+}
