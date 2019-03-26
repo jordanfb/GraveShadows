dda302d66111a00003970f7c4bd57bc44d29a0c4
Eric Franco <france2@rpi.edu>
2019-03-26 19:54:09 -0400

    Adding culprit evidence to a separate list to generate.

--- Assets/Scripts/RandomEvidenceGen.cs
+++ Assets/Scripts/RandomEvidenceGen.cs
@@ -18,10 +18,17 @@ public class RandomEvidenceGen : MonoBehaviour
     private float _spawnPercentage;
 
     private int[] _suspectTotals; //Index corresponds to index of suspect in _allSuspects
+    private List<Evidence> _evidenceToGen;
     // Start is called before the first frame update
     void Start()
     {
+        //Initialize some necessary fields
+        _evidenceToGen = new List<Evidence>();
+        _suspectTotals = new int[5];
+
+        //Gather culprit data to add to evidence to generate list
         int culpritIndex = _allSuspects.IndexOf(_culprit);
+        GatherCulpritEvidence(culpritIndex);
     }
 
     // Update is called once per frame
@@ -32,6 +39,26 @@ public class RandomEvidenceGen : MonoBehaviour
 
     private void GatherCulpritEvidence(int index)
     {
-
+        foreach(Evidence ev in _allEvidence)
+        {
+            //If evidence is assoc. with no one, ignore it
+            if (ev.AssociatedSuspects.Count == 0)
+                continue;
+            if(ev.AssociatedSuspects.Contains(_culprit))
+            {
+                //Add evidence to generation queue if it belongs to culprit
+                _evidenceToGen.Add(ev);
+                _suspectTotals[index]++;
+                //If it points to any other evidence too, add it to the totals
+                if(ev.AssociatedSuspects.Count > 1)
+                {
+                    foreach(Suspect s in ev.AssociatedSuspects)
+                    {
+                        int i = _allSuspects.IndexOf(s);
+                        _suspectTotals[i]++;
+                    }
+                }
+            }
+        }
     }
 }
