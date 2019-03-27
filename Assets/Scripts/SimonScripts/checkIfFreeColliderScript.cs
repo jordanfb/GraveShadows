52b6949e27833f7b907ad511fe79411bdad79afb
Simon Hopkins <simonlhopkins@gmail.com>
2019-03-27 19:49:46 -0400

    added texture for wall selected
    also, added wall colliders into first level. Played with player camera controls

---
+++ Assets/Scripts/SimonScripts/checkIfFreeColliderScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkIfFreeColliderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isColliding;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Evidence")) {
+            return;
+        }
+        isColliding = true;
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        isColliding = false;
+    }
+}
---
+++ Assets/Scripts/SimonScripts/shadowCamScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shadowCamScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Camera shadowCam;
+
+
+    void Awake()
+    {
+
+
+        shadowCam = GetComponent<Camera>();
+        shadowCam.depthTextureMode = DepthTextureMode.Depth;
+
+        shadowCam.SetTargetBuffers(shadowCam.targetTexture.colorBuffer, shadowCam.targetTexture.depthBuffer);
+
+        Shader.SetGlobalTexture("_shadowDepthTexture", shadowCam.targetTexture);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
