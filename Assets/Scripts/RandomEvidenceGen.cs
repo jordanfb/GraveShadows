c629081192050d6acbdcc5de7d7f05690a58c3dc
Eric Franco <france2@rpi.edu>
2019-03-28 14:08:27 -0400

    Random algorithm to queue up 30 pieces of evidence.

--- Assets/Scripts/RandomEvidenceGen.cs
+++ Assets/Scripts/RandomEvidenceGen.cs
@@ -21,18 +21,32 @@ public class RandomEvidenceGen : MonoBehaviour
 
     //Because we don't care about indexing and the random generation ignores duplicates,
     //I figured a set is the most efficient structure to use here.
-    private HashSet<Evidence> _evidenceToGen;
+    private List<Evidence> _evidenceToGen;
     
     // Start is called before the first frame update
     void Start()
     {
         //Initialize some necessary fields
-        _evidenceToGen = new HashSet<Evidence>();
+        _evidenceToGen = new List<Evidence>();
         _suspectTotals = new int[5];
 
         //Gather culprit data to add to evidence to generate list
         int culpritIndex = _allSuspects.IndexOf(_culprit);
-        GatherCulpritEvidence(culpritIndex);
+        GatherCulpritAndConversationEvidence(culpritIndex);
+        GatherRandomEvidence();
+        foreach(Evidence e in _evidenceToGen)
+        {
+            Debug.Log("Evidence: " + e.Name);
+            /*
+            foreach(Suspect s in e.AssociatedSuspects)
+            {
+                Debug.Log("\tAssociated with: " + s.CodeName);
+            }
+            */
+        }
+        foreach (int i in _suspectTotals)
+            Debug.Log(i);
+        Debug.Log("Evidence Count: " + _evidenceToGen.Count);
     }
 
     // Update is called once per frame
@@ -41,12 +55,12 @@ public class RandomEvidenceGen : MonoBehaviour
         
     }
 
-    private void GatherCulpritEvidence(int index)
+    private void GatherCulpritAndConversationEvidence(int index)
     {
         foreach(Evidence ev in _allEvidence)
         {
-            //If evidence is assoc. with no one, ignore it
-            if (ev.AssociatedSuspects.Count == 0)
+            //If evidence is assoc. with no one and isn't a conversation, ignore it
+            if (ev.AssociatedSuspects.Count == 0 && ev.GetEvidenceType != EvidenceType.Conversation)
                 continue;
 
             if(ev.AssociatedSuspects.Contains(_culprit))
@@ -60,9 +74,17 @@ public class RandomEvidenceGen : MonoBehaviour
                 {
                     foreach(Suspect s in ev.AssociatedSuspects)
                     {
-                        int i = _allSuspects.IndexOf(s);
-                        _suspectTotals[i]++;
+                        _suspectTotals[_allSuspects.IndexOf(s)]++;
                     }
+                }
+            }
+            //All conversations need to exist in any playthrough as well
+            else if(ev.GetEvidenceType == EvidenceType.Conversation)
+            {
+                _evidenceToGen.Add(ev);
+                foreach(Suspect s in ev.AssociatedSuspects)
+                {
+                    _suspectTotals[_allSuspects.IndexOf(s)]++;
                 }
             }
         }
@@ -76,18 +98,34 @@ public class RandomEvidenceGen : MonoBehaviour
             return;
         }
 
+        List<Evidence> remainingEv = new List<Evidence>(_allEvidence);
+        foreach (Evidence e in _evidenceToGen)
+            remainingEv.Remove(e);
+
         //Apparently not using the System namespace calls a vestigial UnityEngine Random class instead.
         //Also didn't want to use all of System AND either way I still needed to specify bc C# got angry.
         System.Random rng = new System.Random();
 
-        //Is this efficient??? God I hope so.
-        //Need to refactor
-        while ( _evidenceToGen.Count < _evidenceTotal)
+        while(_evidenceToGen.Count < _evidenceTotal)
         {
-            int next = rng.Next(0, _allEvidence.Count - 1);
-            Evidence e = _allEvidence[next];
-            _allEvidence.Add(e);
-            
+            int next = rng.Next(0, remainingEv.Count - 1);
+            Evidence randEv = remainingEv[next];
+            if(CheckIfPlaceable(randEv))
+            {
+                foreach (Suspect s in randEv.AssociatedSuspects)
+                    _suspectTotals[_allSuspects.IndexOf(s)]++;
+                _evidenceToGen.Add(randEv);
+            }
         }
+    }
+
+    private bool CheckIfPlaceable(Evidence ev)
+    {
+        foreach(Suspect s in ev.AssociatedSuspects)
+        {
+            if (_suspectTotals[_allSuspects.IndexOf(s)] >= _otherSuspectMax)
+                return false;
+        }
+        return true;
     }
 }
