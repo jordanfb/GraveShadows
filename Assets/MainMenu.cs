59a1dc692c497132d33ef328a9fa0cbf4ebc498d
Unknown <thezschwartz@gmail.com>
2019-04-03 04:08:10 -0400

    Created main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame");
+        Application.Quit();
+    }
+}
