e3450b9a4aea7273faf867c53d7bade8cd12603a
Simon Hopkins <simonlhopkins@gmail.com>
2019-04-03 00:41:27 -0400

    added open close doors and turn on off lights

---
+++ Assets/Scripts/SimonScripts/breakerBoxScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakerBoxScript : MonoBehaviour
+{
+
+    public List<GameObject> affiliatedLights;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject l in affiliatedLights)
+        {
+            l.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            if (Input.GetKeyDown(KeyCode.E)) {
+                foreach (GameObject l in affiliatedLights)
+                {
+                    l.SetActive(true);
+                }
+            }
+
+        }
+    }
+}
