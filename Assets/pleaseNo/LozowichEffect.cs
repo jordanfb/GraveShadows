1ac6dfc0d1ad9bd528f784514bc6b536b050983e
Simon Hopkins <simonlhopkins@gmail.com>
2019-04-09 19:57:59 -0400

    COMPLETELY revamped how I am handleing the TAM

---
+++ Assets/pleaseNo/LozowichEffect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LozowichEffect : MonoBehaviour {
+
+    public float intensity;
+    public float tileAmount = 1f;
+    private Material material;
+
+    // Creates a private material used to the effect
+    void Awake ()
+    {
+        material = new Material( Shader.Find("Hidden/LozowichEffectShader") );
+    }
+    
+    // Postprocess the image
+    void OnRenderImage (RenderTexture source, RenderTexture destination)
+    {
+        if (intensity == 0)
+        {
+            Graphics.Blit (source, destination);
+            return;
+        }
+
+        material.SetFloat("_bwBlend", Mathf.Clamp(intensity,0f, 1f)); 
+        material.SetFloat("_tile", Mathf.Clamp(tileAmount, 0f, 10f));
+        Graphics.Blit (source, destination, material);
+    }
+}
