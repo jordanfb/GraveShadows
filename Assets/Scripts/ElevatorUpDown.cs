7f095903d75ffabe1738259e3cfcc87a800b3986
Grant Doney <gdoney1701@gmail.com>
2019-04-15 18:46:18 -0400

    Elevator Function Added
    Now you can't fall off

--- Assets/Scripts/ElevatorUpDown.cs
+++ Assets/Scripts/ElevatorUpDown.cs
@@ -5,70 +5,65 @@ using UnityEngine;
 
 public class ElevatorUpDown : MonoBehaviour
 {
-    public float speed;
-    private bool goingUp;
+    private bool moving;
     public bool isOn = false;
     private Vector3 maxHeight;
     private Vector3 minHeight;
+    public GameObject childGate;
+    public GameObject topGate;
+    public GameObject bottomGate;
     // Start is called before the first frame update
     void Start()
     {
-        goingUp = false;
+        moving = false;
         maxHeight = transform.position;
         minHeight = new Vector3(transform.position.x, 0f, transform.position.z);
-        //LetsMove();
+        LetsMove();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
-
     }
    public void LetsMove()
     {
         StartCoroutine(Movement2(transform.position, maxHeight, minHeight, 3f));
     }
-    //IEnumerator Movement(float whereAmI)
-    //{
-      //  print(" I Am " + whereAmI);
-     //   print("maxHeight =" + maxHeight);
-      //  if (whereAmI.y >= maxHeight.y || whereAmI.y <= minHeight.y)
-      //  {
-      //      speed *= -1;
-      //      yield return new WaitForSeconds(3);
-      //      transform.position = new Vector3(transform.position.x, whereAmI + speed, transform.position.z);
-
-
-     //   }
-
-       // transform.position = new Vector3(transform.position.x, whereAmI + speed, transform.position.z);
-
-   // }
     IEnumerator Movement2(Vector3 whereIStart,  Vector3 max, Vector3 min, float time)
     {
-        int counterman = 0;
-        while (counterman < 10)
+        //da big loop
+        moving = true;
+        while (moving == true)
         {
             float elapsedTime = 0f;
             whereIStart = transform.position;
-            while (elapsedTime < time)
+            //going down
+            while (elapsedTime < time) 
             {
                 transform.position = Vector3.Lerp(whereIStart, min, (elapsedTime / time));
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
+            childGate.SetActive(false);
+            bottomGate.SetActive(false);
             yield return new WaitForSeconds(2);
+            childGate.SetActive(true);
+            bottomGate.SetActive(true);
             elapsedTime = 0f;
             whereIStart = transform.position;
+            //going up
             while (elapsedTime < time)
             {
                 transform.position = Vector3.Lerp(whereIStart, max, (elapsedTime / time));
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
+            childGate.SetActive(false);
+            topGate.SetActive(false);
             yield return new WaitForSeconds(2);
+            childGate.SetActive(true);
+            topGate.SetActive(true);
             yield return null;
         }
 
