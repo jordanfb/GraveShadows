a68403f1a7920b31436e93b8d0f0bf2e23ce346f
Eric Franco <france2@rpi.edu>
2019-04-16 00:26:55 -0400

    Added SFX to win/loss scenes, timer for multiple shot SFX on loss

---
+++ Assets/Scripts/LossAudioTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LossAudioTimer : MonoBehaviour
+{
+    public float delay;
+    [HideInInspector]
+    public AudioSource myAudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+        StartCoroutine(PlayAudio());
+    }
+
+    IEnumerator PlayAudio()
+    {
+        yield return new WaitForSeconds(delay);
+        myAudioSource.Play();
+    }
+}
