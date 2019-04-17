a4b52c2659e5d1fbff174d0b87fbf2a32be54973
Jordan Faas-Bush <jmanfb@gmail.com>
2019-04-17 02:35:15 -0400

    custom level 1 logic

---
+++ Assets/CustomLevel1Logic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CustomLevel1Logic : MonoBehaviour
+{
+    public UnityEvent onAllEvidenceCollected;
+
+    int numCollected = 0;
+    public int numEvidence = 0;
+
+    public void CollectedEvidence()
+    {
+        numCollected++;
+        if (numCollected >= numEvidence)
+        {
+            onAllEvidenceCollected.Invoke();
+        }
+    }
+}
