5cc81fc26ba76d4fd972060f1e9b4fbba94df362
Simon Hopkins <simonlhopkins@gmail.com>
2019-04-23 22:47:12 -0400

    added ive got all evidence mono

---
+++ Assets/Scripts/customLevel1/spawnGotEverything.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnGotEverything : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject ev1;
+    public GameObject ev2;
+    public GameObject gotEverythingCollider;
+
+    void Start()
+    {
+        gotEverythingCollider.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ev1 == null && ev2 == null) {
+            gotEverythingCollider.SetActive(true);
+        }
+    }
+}
