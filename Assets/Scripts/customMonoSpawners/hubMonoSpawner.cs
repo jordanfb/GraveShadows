d2885800bd2d2dad7e6348be0152b9e4799bee34
Simon Hopkins <simonlhopkins@gmail.com>
2019-04-23 23:38:36 -0400

    added monologue to hub

---
+++ Assets/Scripts/customMonoSpawners/hubMonoSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hubMonoSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject col1;
+    public GameObject col2;
+
+    void Start()
+    {
+        if (GameplayManager.instance.dayNum < 2) {
+            col1.SetActive(true);
+            col2.SetActive(true);
+        }
+        else {
+            col1.SetActive(false);
+            col2.SetActive(false);
+
+        }
+    }
+}
