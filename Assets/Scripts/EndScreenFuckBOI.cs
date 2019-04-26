2f65f959df8900dee24849df392bb78d3997fb06
Grant Doney <gdoney1701@gmail.com>
2019-04-25 23:17:50 -0400

    New EndScene with more assets

---
+++ Assets/Scripts/EndScreenFuckBOI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScreenFuckBOI : MonoBehaviour
+{
+    public GameObject Head1;
+    public GameObject Subhead1;
+    public GameObject Head2;
+    public GameObject Subhead2;
+    public GameObject MrButton;
+    public GameObject bigBad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string bBN = EvidenceManager.instance.culprit.Name;
+        print(bBN);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
