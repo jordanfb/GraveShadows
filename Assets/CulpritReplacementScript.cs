202c5513b286f7b2c1fbef382357028918e528d9
Jordan Faas-Bush <jmanfb@gmail.com>
2019-04-27 13:03:51 -0400

    Added culprit to lose screen and fixed teleport to demo level 2

---
+++ Assets/CulpritReplacementScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CulpritReplacementScript : MonoBehaviour
+{
+    public string toReplace = "CULPRIT";
+    public Text textToReplaceIn;
+
+    string culprit = "CULPRIT";
+    // Start is called before the first frame update
+    void Start()
+    {
+        culprit = EvidenceManager.instance.culprit.Name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textToReplaceIn.text = textToReplaceIn.text.Replace(toReplace, culprit);
+    }
+}
