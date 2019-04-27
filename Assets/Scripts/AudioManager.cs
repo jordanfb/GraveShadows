86a443b90d6b51c2694dd5de34e1d6c8fde6ca51
Jordan Faas-Bush <jmanfb@gmail.com>
2019-04-27 01:47:43 -0400

    Got audioscript ready for sprinting audio clip

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -20,6 +20,11 @@ public class AudioManager : MonoBehaviour
         _audioSource = GetComponent<AudioSource>();
     }
 
+    private bool IsSprinting()
+    {
+        return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+    }
+
     // Update is called once per frame
     private void Update()
     {
@@ -28,6 +33,12 @@ public class AudioManager : MonoBehaviour
             !_audioSource.isPlaying && !_srManager.isInShadowRealm)
         {
             _audioSource.clip = _clips[0];
+            //if (IsSprinting())
+            //{
+            //    // chose a different clip here
+            //    _audioSource.clip = _clips[2];
+            //}
+            _audioSource.volume = 1f;
             _audioSource.Play();
         }
 
