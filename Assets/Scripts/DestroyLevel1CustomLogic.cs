1ed24f1a4984f620325eb6f1fb2a9e4b632855d4
Jordan Faas-Bush <jmanfb@gmail.com>
2019-05-01 16:00:55 -0400

    Created destroy level 1 evidence manager script

---
+++ Assets/Scripts/DestroyLevel1CustomLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyLevel1CustomLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // destroy the level 1 custom logic
+        LevelOneEvidenceManager level1CustomLogic = FindObjectOfType<LevelOneEvidenceManager>();
+        if (level1CustomLogic != null)
+        {
+            Destroy(level1CustomLogic.gameObject);
+            LevelOneEvidenceManager.instance = null;
+        }
+    }
+}
