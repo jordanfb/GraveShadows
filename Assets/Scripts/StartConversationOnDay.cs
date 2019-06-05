d64afc8b7cbc5af4cf240c44bece4064910d5ab3
Jordan Faas-Bush <jmanfb@gmail.com>
2019-06-05 02:07:52 -0400

    Added conversation that starts on first day in office

---
+++ Assets/Scripts/StartConversationOnDay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartConversationOnDay : MonoBehaviour
+{
+    // This is a monobehavior to give a public function that will start a conversation but only on a certain day (or excluding a certain day)
+    // this is for creating "first day: I should try to find as much evidence as possible but avoid those guards!" and maybe a last day too
+
+    public enum modeSpecificFunctionality
+    {
+        allModes, demoModeOnly, fullGameOnly
+    }
+
+
+    public int dayNum = 0;
+    public string conversationName = "";
+    public modeSpecificFunctionality validModes = modeSpecificFunctionality.allModes;
+
+    private ConversationMetaManager manager;
+
+    public void TryStartConversation()
+    {
+        if (GameplayManager.instance.dayNum == dayNum && (validModes == modeSpecificFunctionality.allModes || (Options.instance.demoMode == (validModes == modeSpecificFunctionality.demoModeOnly)) ) )
+        {
+            // only run on the specific day and if it's the correct mode
+            if (manager != null && conversationName.Length > 0)
+            {
+                // it's possible to play the convo
+                manager.PlayConversationOfName(conversationName);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<ConversationMetaManager>();
+    }
+}
