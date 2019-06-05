f1ca18255fb8157ac8fae7b5e12f7e04a91ad0e8
Jordan Faas-Bush <jmanfb@gmail.com>
2019-06-05 02:35:13 -0400

    Tried to make the scroll bar work but it's not gonna

---
+++ Assets/Scripts/ScrollToZeroOnEnable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollToZeroOnEnable : MonoBehaviour
+{
+    public Scrollbar bar;
+    public RectTransform view;
+
+    private void OnEnable()
+    {
+        // set the scroll bar value to zero I guess :/
+        bar.enabled = false;
+        bar.size = 0;
+        bar.value = 0;
+        view.localPosition = Vector3.zero;
+        bar.enabled = true;
+    }
+}
